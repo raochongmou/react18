bb229bc049bfb3ba623f65fb843e05cb087a58db
raochongmou <1041060542@qq.com>
2023-04-05 22:28:17 +0800

    r-c-m

---
+++ react18/day01/data/index.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    id: 1,
+    bookName: "《算法与导论》",
+    date: "2005-9",
+    price: 85,
+    count: 1
+  },
+  {
+    id: 2,
+    bookName: "《如何放弃react》",
+    date: "2025-9",
+    price: 195,
+    count: 2
+  },
+  {
+    id: 3,
+    bookName: "《vue如何学不会》",
+    date: "2035-9",
+    price: 205,
+    count: 5
+  },
+  {
+    id: 4,
+    bookName: "《angular怎样学》",
+    date: "2015-9",
+    price: 185,
+    count: 10
+  }
+];
